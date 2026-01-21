0cabbd91ba7f1698da6390752485b678b26f69c2
bacchinl <lucie.bacchin@gmail.com>
2026-01-21 19:55:06 +0100

    Update buttressing_Furst.py

--- CODES_FOR_PLOTS/POST_ELMER/buttressing_Furst.py
+++ CODES_FOR_PLOTS/POST_ELMER/buttressing_Furst.py
@@ -1,9 +1,20 @@
+
+""" This code works with the plot_buttressing functions """
+
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm
 import numpy as np
 import pyvista as pv
 import matplotlib.tri as mtri
+from plot_buttressing import (
+    plot_buttressing,
+    plot_corrected,
+    plot_stress,
+    get_furst_colormap,
+    test_var
+)
 
 small = 1e-10
 year = 2016
@@ -57,7 +68,7 @@ def compute_buttressing_unstructured(filename, nn=3, rhoice=917, rhow=1028, grav
     dvdy = grad_vy[:,1]
 
     # ---------------------------------------------------------
-    # 2) TENSEUR DE DÉFORMATION
+    # 2) TENSEUR DE DÉFORMATION (epsilon)
     # ---------------------------------------------------------
     exx = dudx
     eyy = dvdy
@@ -68,7 +79,7 @@ def compute_buttressing_unstructured(filename, nn=3, rhoice=917, rhow=1028, grav
     eff = 0.5*(exx**2 + eyy**2 + ezz**2) + exy**2 + small
 
     # ---------------------------------------------------------
-    # 3) DEVIATORIC STRESS
+    # 3) DEVIATORIC STRESS (glen law)
     # ---------------------------------------------------------
     rf = eta  # viscosité, stockée en racine dans pvtu
     factor = rf**(-1/nn) * eff**((1-nn)/(2*nn))
@@ -78,7 +89,7 @@ def compute_buttressing_unstructured(filename, nn=3, rhoice=917, rhow=1028, grav
     ttxy = exy * factor
 
     # ---------------------------------------------------------
-    # 4) TENSEUR T MODIFIÉ
+    # 4) TENSEUR T MODIFIÉ (stress tensor in SSA) 
     # ---------------------------------------------------------
     t11 = 2*ttxx + ttyy
     t12 = ttxy
@@ -119,114 +130,12 @@ def compute_buttressing_unstructured(filename, nn=3, rhoice=917, rhow=1028, grav
     KN2 = 1 - eig1 / tau0
     KN3 = 1 - eig2 / tau0
 
-    return x, y, vx, vy, KT, KN1, KN2, KN3
-#------------------------------
-#   PLOT
-#------------------------------
-# palette comme dans furst et al 
-
-colors = [
-    "#39A0CD","#54B6E0","#6BC9F2","#85D5F0", "#9BD5C5", "#B1D295", "#CACF67","#E1CE34","#F7CD19", "#E9BD1F", "#D0AA2C","#B79530"
-]
-color_fuerst = LinearSegmentedColormap.from_list("color_fuerst", colors, N=12)
-
-def plot_buttressing(x, y, KT, KN1, KN2, KN3, color_fuerst, year):
-
-    fig, axes = plt.subplots(2, 2, figsize=(14, 12))
-    maps = [
-        (KT,  "KT – transverse"),
-        (KN1, "KN1 – along-flow"),
-        (KN2, "KN2 – eigenvalue 1"),
-        (KN3, "KN3 – eigenvalue 2"),
-    ]
-
-    for ax, (K, title) in zip(axes.ravel(), maps):
-
-        scatter = ax.scatter(
-            x, y,
-            c=K,
-            cmap=color_fuerst,
-            vmin=0, vmax=1,
-            marker='.',
-            s=1,
-            alpha=1
-        )
-
-        ax.set_title(f"{title} ({year})")
-        ax.set_xlabel("X")
-
-        # label Y uniquement sur la colonne de gauche
-        if ax in axes[:,0]:
-            ax.set_ylabel("Y")
-
-        fig.colorbar(scatter, ax=ax, label="buttressing number")
-
-
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(f"./Subplot_buttressing_Furst_{year}", dpi = 300)
-
-
-def plot_corrected(x, y, KN3, vx, vy, color_f, title="Corrected KN3"):
-    """
-    Make it look like Furst et al 2016
-    - KN3 * 1 / sign(vm)
-    - niveaux [-10 -2 -0.5 0:0.1:1 2 4 100]
-    - colormap blue–brown (déjà définie dans color_f)
-    - contourf non linéaire, style MATLAB
-    """
-
-     # ---------------------------
-    # Calcul de vmELM = magnitude du vecteur vitesse
-    # ---------------------------
-    vm = np.sqrt(vx**2 + vy**2)
-    vm[vm == 0] = 1e-12  # éviter division par zéro
-
-    # ---------------------------
-    # TRICK MATLAB
-    # ---------------------------
-    Kn_plot = (KN3 / vm)*100    # pas de shelfmask car tu es sur un seul ice shelf
-    test_var(Kn_plot, "Kn_plot (KN corrected)")
-    # ---------------------------
-    # same contour levels
-    # ---------------------------
-    levels = [-10, -2, -0.5] + list(np.arange(0, 1.1, 0.1)) + [2, 4, 100]
-
-    # ---------------------------
-    # figure
-    # ---------------------------
-    fig, ax = plt.subplots(figsize=(8, 6))
-
-    scatter = ax.scatter(
-        x, y,
-        c=Kn_plot,
-        cmap=color_f,
-        marker='.',
-        s=1,
-        alpha=1,
-        vmin=0, vmax=1  # tu peux adapter la plage
-    )
-    # ---------------------------
-    # colorbar
-    # ---------------------------
-    cbar = fig.colorbar(scatter, ax=ax)
-    cbar.set_label("Corrected Kn")
-
-    # ---------------------------
-    # labels, title
-    # ---------------------------
-    ax.set_aspect("equal")
-    ax.set_title(title)
-    ax.set_xlabel("X (m)")
-    ax.set_ylabel("Y (m)")
-
-    return fig, ax
+    return x, y, vx, vy, t11, t12, t21, t22, KT, KN1, KN2, KN3
 
 
 
-##### LAUNCH #####
 
-x, y, vx, vy, KT, KN1, KN2, KN3 = compute_buttressing_unstructured(filename)
+x, y, vx, vy, t11, t12, t21, t22, KT, KN1, KN2, KN3 = compute_buttressing_unstructured(filename)
 
 ## tests 
 def test_var(var, name):
@@ -252,7 +161,13 @@ test_var(KN2, "KN2 (eigenvalue 1)")
 test_var(KN3, "KN3 (eigenvalue 2)")
 
 
+#------------------------------
+#   PLOT
+#------------------------------
+# palette comme dans furst et al 
 
+
+cmap = get_furst_colormap()
 
 #plot_buttressing(x, y, KT, KN1, KN2, KN3, color_fuerst, year)
 
@@ -260,8 +175,16 @@ fig, ax = plot_corrected(
     x, y,
     KN3,
     vx,vy,
-    color_fuerst,        # ta palette blue–brown ou custom
+    cmap,        # ta palette blue–brown ou custom
     title="KNflow – Corrected Version - Friction at {beta}"
 )
-#plt.savefig(f"./buttressing/buttressing_corrected_{year}_FRT_{beta}", dpi = 300)
+plt.savefig(f"../BUTTRESSING/buttressing_corrected_{year}_FRT_{beta}", dpi = 300)
+plt.show()
+
+
+fig, ax = plot_stress(
+    x, y,t11, t12, t21, t22, year, beta
+)
+#plt.title(f"Stress for year {year}, friction param of {beta}")
+plt.savefig(f"../STRESS/stress_{year}_FRT_{beta}", dpi = 300)
 plt.show()
